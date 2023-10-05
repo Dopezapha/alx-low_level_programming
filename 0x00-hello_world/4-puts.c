d2e6b167ec56dcb6de364a17c2e7cc2038080758
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-05 11:01:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-	char mystring[] = ("\"Programming is like building a multilingual puzzle");
-
-	puts(mystring);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

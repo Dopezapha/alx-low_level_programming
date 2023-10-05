1adec6c50e3adc2593cf873400805a64c91d811f
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-05 10:40:44 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mystring[] = ("Programming is like building a multilingual puzzle\n");
+
+	puts(mystring);
+
+	return (0);
+}

af492085b4799b4b349df3e0938374b4d9a8254d
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-06 03:08:26 -0700

    101-quote.c modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* Main - The c program that prints to stnadard error.
+* Main - Entry point
+*
+* Description: The c program that prints to stnadard error.
 *
 * Return: Always 1 (success)
 */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	
-	return(1);
+	return (1);
 }

731e6f898255d9d3b11b65e26dc1f2ec09d44fcb
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-06 03:59:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
 *
 * Return: Always 1 (success)
 */
-int main(void)
+int main(void);
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19" , stdout);
+	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }

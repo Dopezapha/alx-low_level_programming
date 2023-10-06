0378b375d09860636f6a15bfa21ae89a8876f818
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-06 03:17:59 -0700

    101-quote.c modified 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
-* main - Entry point
-*
-* Description: The c program that prints to standard error.
-*
-* Return: Always 1 (success)
-*/
+ * main - Entry point
+ *
+ * Description: The C program that prints a message to standard error.
+ *
+ * Return: Always 1 (success)
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+    fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+    return (1);
 }

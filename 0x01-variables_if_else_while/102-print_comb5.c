d89b236260d35239c0480a516c38e44250d4e0cb
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-08 12:07:38 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: A program that prints all possible
+*
+*combinations of two two-digit numbers.
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+	int a;
+	int b;
+
+	for (a = 0; a <= 99; a++)
+	{
+
+	for (b = a; b <= 99; b++)
+	{
+
+		if (a != b)
+		{
+		putchar((a / 10) + '0');
+		putchar((a % 10) + '0');
+		putchar(' ');
+		putchar((b / 10) + '0');
+		putchar((b % 10) + '0');
+
+		if (a != 98 || b != 99)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+

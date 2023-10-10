32ac6490ff2c9aa0606b79a87c6ea5fac832fe5e
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-10 06:15:15 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Code that prints _putchar
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	for (a = 0; c[a] != '\0'; a++)
+
+	{
+	_putchar(c[]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}

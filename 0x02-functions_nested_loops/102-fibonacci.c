fea6749a1b4692be05b5d28af70750f9ca686f1c
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-11 13:45:40 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,7 +2,6 @@
 
 /**
 * main - A program that prints the first 50 Fibonacci
-*
 * numbers, starting with 1 and 2.
 *
 * Return: 0
@@ -15,11 +14,12 @@ int main(void)
 	unsigned long int next;
 	int c;
 
-	printf("%lu, ", a);
-	for (c = 1; c < 50; c++)
+	printf("%lu, %lu, ", a, b);
+	for (c = 2; c < 50; c++)
 	{
-	printf("%lu", b);
 	next = a + b;
+	printf("%lu", next)
+	a = b;
 	b = next;
 	if (c != 49)
 	printf(", ");

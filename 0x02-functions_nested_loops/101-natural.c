2c11b02255ff663fdcef259f65bc42fd8bea6755
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-11 12:09:02 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,48 @@
 #include "main.h"
 
 /**
-* main - checks the code.
-*
-* Return: Always 0.
+* print_times_table - print a times table for a given number
+* @n: The value for the times table
 */
-int main(void)
+
+void print_times_table(int n)
 {
-	
-	print_times_table(3);
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+	for (num = 0; num <= n; num++)
+	{
+	for (mult = 0; mult <= n; mult++)
+		{
+		prod = num * mult;
+		if (mult > 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		if (prod <= 9)
+		_putchar(' ');
+		}
+		if (prod <= 99)
+		_putchar(' ');
+		if (prod >= 100)
+		{
+		_putchar((prod / 100) + '0');
+		_putchar(((prod / 10) % 10) + '0');
+		}
+		else if (prod >= 10)
+		{
+		_putchar(' ');
+		_putchar((prod / 10) + '0');
+		}
+		else if (mult > 0)
+		{
+		_putchar(' ');
+		_putchar(' ');
+		}
+		_putchar((prod % 10) + '0');
+		}
 	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
+	}
+	}
 }

86879cd317645313aa4a28ca994a98f78180c891
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-12 11:12:43 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+*mul - Multiply two integers.
+*@a: integer to be multiplied
+*@b: integer to be multiplied
+*Return: Always 0
+*/
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	c = a * b
+	{
+	i_putchar(c + '0');
+	_putchar('\n');
+	}
+	return (0);
+}

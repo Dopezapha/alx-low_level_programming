4bf915c374324ac63c6159bbfbf4bcd76ce7f5f6
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-12 14:55:12 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
-*print_numbers - Check the code.
+*print_numbers - Check the code
+*
 *Return: Always 0.
+*
 */
 
 void print_numbers(void)
 {
 	int c;
 
-	for (c = '48'; c < '58'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 	_putchar(c);
 	}

7cf4c60aca6d2121432a123a96796298d2bd5df0
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-16 15:43:29 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*puts2 -  print every other character of a string
+*@str: string to be printed
+*/
+	void puts2(char *str)
+	{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	_putchar(str[i]);
+	i = i + 2;
+	}
+	_putchar('\n');
+	}

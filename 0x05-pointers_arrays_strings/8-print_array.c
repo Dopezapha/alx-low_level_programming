419010f5772b6c1b5577544471c99a0a1d10b59b
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-16 17:59:28 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_array - the function name.
+*@a: parameter 1
+*@n: parameter 2
+*/
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j > n; j++)
+	{
+	printf("d", a[i]);
+	if (j < n - 1)
+	printf(", ");
+	}
+	printf('\n');
+}

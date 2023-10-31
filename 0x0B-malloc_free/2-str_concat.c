4ff99f073aba56bb76082a77f16f31fe77fcb403
Dopezapha <officialnwaneridaniel@gmail.com>
2023-10-31 09:31:32 -0700

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+
 /**
 * str_concat - concatenate two strings.
 * @s1: string to be considered
@@ -9,26 +10,33 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int i;
-	int j;
-	int len1;
-	int len2;
+	int i = 0;
+	int j = 0;
+	int len1 = 0;
+	int len2 = 0;
 
-	if (s1 != NULL)	
+	if (s1 != NULL)
+		{
 		len1 = strlen(s1);
+		}
 	if (s2 != NULL)
+		{
 		len2 = strlen(s2);
-
-	ptr = malloc((len1 + len2) * sizeof(char) + 1);
+		}
+	ptr = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (ptr == NULL)
+		{
 		return (NULL);
-
-	for (i = 0; s1[i] != '\0'; i++)
+		}
+	for (i = 0; s1 != NULL && s1[i] != '\0'; i++)
+		{
 		ptr[i] = s1[i];
-	for (j = 0; s2[j] != '\0'; j++)
-		ptr[i + j] = s2[j];
-
-	ptr[len1 + len2] = '\0';
+		}
+	for (j = 0; s2 != NULL && s2[j] != '\0'; j++)
+	{
+	ptr[i + j] = s2[j];
+	}
+	ptr[i + j] = '\0';
 	return (ptr);
 }

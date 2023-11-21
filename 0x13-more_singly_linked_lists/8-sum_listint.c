4e2843cf803993da315b82c220af90df59622839
Dopezapha <officialnwaneridaniel@gmail.com>
2023-11-21 04:41:39 -0800

    linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+*sum_listint - Calculates the sum of integer values in a listint_t linked list.
+*@head: Pointer to the head of the listint_t list.
+*Return: The sum of integer values in the linked list, or 0 if the list is empty.
+*/
+int sum_listint(listint_t *head)
+{
+int sum = 0;  /* Use an integer variable to store the sum */
+
+if (head == NULL)
+return 0;  /* Return 0 for an empty list */
+
+/* Traverse the list and add up the values */
+while (head != NULL)
+{
+sum += head->n;  /* Add the value of the current node to the sum */
+head = head->next;  /* Move to the next node */
+}
+
+return (sum);  /* Return the calculated sum */
+}
+

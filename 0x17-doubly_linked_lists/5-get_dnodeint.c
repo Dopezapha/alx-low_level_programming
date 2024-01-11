98cfd5ef8a3a01b4d31779360c2203e66c59dab0
Dopezapha <officialnwaneridaniel@gmail.com>
2024-01-11 07:46:32 -0800

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -9,9 +9,10 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 dlistint_t *current = head;
+unsigned int i;
 
 /* Traverse the list until reaching the desired index or the end of the list */
-for (unsigned int i = 0; current != NULL && i < index; i++)
+for (i = 0; current != NULL && i < index; i++)
 {
 current = current->next;
 }
